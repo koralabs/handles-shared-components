8fa0a8ca641a0896b1ad523e10c60598a0eaefad
Tan <tanarin12@gmail.com>
2023-10-11 13:46:51 -0400

    Add background, style buttongroup and loader

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,48 +1,115 @@
-import React from 'react';
-import './button.css';
+import React from "react";
+import Fade from "../Fade";
+import { Loader } from "../Loader";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  buttonStyle?:
+    | "primary"
+    | "secondary"
+    | "tertiary"
+    | "link"
+    | "outline"
+    | "green";
+  internal?: boolean;
+  href?: string;
+  className?: string;
+  disabled?: boolean;
+  loading?: boolean;
+  animate?: boolean;
+  icon?: string;
+  children?: JSX.Element | JSX.Element[] | string | string[];
+  [key: string]: unknown;
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+const renderIcon = (iconName: string) => {
+  if (iconName) {
+    console.log('they be lookin for an icon called "' + iconName + '"yo');
+    return "<" + { iconName } + "/>";
+  } else {
+    console.log("no need for an icon on this button dawg");
+  }
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  className = "",
+  href,
+  internal = true,
+  buttonStyle = "secondary",
+  animate = false,
+  icon = "",
+  children,
+  disabled,
+  loading = false,
+  ...rest
+}): JSX.Element => {
+  // const classes = ['cursor-pointer hover:shadow-lg m-0 py-2 px-6 text-center rounded-3xl inline-block font-bold'];
+
+  // if (disabled) {
+  //     classes.push('cursor-not-allowed bg-dark-300 hover:bg-dark-300 active:bg-dark-300 text-dark-350');
+  // }
+
+  // if (animate) {
+  //     classes.push('transform hover:translate-x-2');
+  // }
+
+  // console.log('buttonStyle', buttonStyle)
+
+  // if (!disabled) {
+  //     switch (buttonStyle) {
+  //         case 'primary':
+  //             classes = 'cursor-pointer hover:shadow-lg m-0 py-2 px-6 text-center rounded-3xl inline-block font-bold bg-primary-200 hover:bg-dark-100 active:bg-dark-100 text-dark-100';
+  //         case 'secondary':
+  //             classes = 'cursor-pointer hover:shadow-lg m-0 py-2 px-6 text-center rounded-3xl inline-block font-bold bg-primary-100 hover:bg-primary-200 active:bg-dark-100 text-white';
+  //         case 'tertiary':
+  //             classes = 'cursor-pointer hover:shadow-lg m-0 py-2 px-6 text-center rounded-3xl inline-block font-bold bg-primary-100 hover:bg-primary-200 active:bg-dark-100 text-white';
+  //         case 'outline':
+  //             classes = 'cursor-pointer hover:shadow-lg m-0 py-2 px-6 text-center rounded-3xl inline-block font-bold bg-dark-200 border border-dark-225 text-white';
+  //         case 'green':
+  //             classes = 'cursor-pointer hover:shadow-lg m-0 py-2 px-6 text-center rounded-3xl inline-block font-bold bg-green-100 border border-dark-225 text-white';
+  //         default:
+  //             classes = 'cursor-pointer hover:shadow-lg m-0 py-2 px-6 text-center rounded-3xl inline-block font-bold bg-primary-100 hover:bg-primary-200 active:bg-dark-100 text-white';
+  //     }
+  // }
+
+  const classes = `cursor-pointer hover:shadow-lg m-0 py-2 px-6 text-center rounded-3xl inline-block font-bold transition-all ${className}`;
+
+  if (internal && href?.length) {
+    return (
+      <a href={href}>
+        <a className={classes + " " + className} {...rest}>
+          {children}
+        </a>
+      </a>
+    );
+  }
+
+  if (!internal && href?.length) {
+    return (
+      <a href={href} className={classes + " " + className} {...rest}>
+        {children}
+      </a>
+    );
+  }
+
+  if (loading) {
+    return (
+      <button className={classes + " " + className} disabled={true} {...rest}>
+        <div className="h-8 invert">
+          <div className="-mt-3.5">
+            <Fade fadeKey={"loaderupgrade"} fadeDuration={0.2}>
+              <Loader scale={0.5} />
+            </Fade>
+          </div>
+        </div>
+      </button>
+    );
+  }
+
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
+    <button className={classes + " " + className} disabled={disabled} {...rest}>
+      {children}
     </button>
   );
 };
+
+export default Button;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Button } from "./components/Button";
 import { Loader } from "./components/Loader";
+import ButtonGroup from "./components/ButtonGroup";
 
-export { Button, Loader };
+export { Button, Loader, ButtonGroup };
