762e60827919e7383de7b89bc58a1dd17101ca6a
Tan <tanarin12@gmail.com>
2023-10-24 12:13:51 -0400

    Add more custom components witha args

---
+++ src/components/CustomTooltip/CustomSelect.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import CustomTooltip from ".";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: any = {
+  title: "Example/CustomTooltip",
+  component: CustomTooltip,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} satisfies Meta<typeof CustomTooltip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Main: Story = {
+  args: {
+    children: "Test",
+    label: "Test Tooltip",
+  },
+};
