2e6709258aa4a78a3269ae758e802204b6b0e795
Tan <tanarin12@gmail.com>
2024-10-10 15:49:21 -0700

    Fix loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+import loader1 from '../../assets/loader1.svg';
+import loader2 from '../../assets/loader2.svg';
+
+
 interface LoaderProps {
   size?: number;
   className?: string;
@@ -10,55 +14,52 @@ export const Loader = ({ size = 48, className = "" }: LoaderProps) => {
   const innerSize = size / 2;
   const innerOffset = size / 4;
 
+  const containerStyle: React.CSSProperties = {
+    width: size,
+    height: size,
+    margin: "auto",
+    position: "relative",
+  };
+
+  const outerSvgStyle: React.CSSProperties = {
+    width: size,
+    height: size,
+    position: "absolute",
+    alignItems: "left",
+  };
+
+  const innerSvgStyle: React.CSSProperties = {
+    width: innerSize,
+    height: innerSize,
+    position: "absolute",
+    left: innerOffset,
+    top: innerOffset,
+  };
+
+  const svgFullHeightStyle: React.CSSProperties = {
+    height: "100%",
+  };
+
+  const svgFullHeightMarginLeftAutoStyle: React.CSSProperties = {
+    height: "100%",
+    marginLeft: "auto",
+  };
+
   return (
-    <div
-      className={`m-auto relative ${className}`}
-      style={{ width: size, height: size }}
-    >
+    <div style={containerStyle} className={className}>
       <motion.div
-        className="align-left absolute"
-        style={{ width: size, height: size }}
+        style={outerSvgStyle}
         animate={{ rotate: 360 }}
         transition={{ ease: "linear", duration: 2, repeat: Infinity }}
       >
-        <svg
-          className="h-full"
-          width="25"
-          height="49"
-          viewBox="0 0 25 49"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M24.9883 47.0966C24.9883 47.7594 24.4507 48.2997 23.7888 48.2666C21.0469 48.1294 18.3457 47.5226 15.8039 46.4697C12.8921 45.2636 10.2463 43.4958 8.01772 41.2672C5.78911 39.0386 4.02128 36.3928 2.81517 33.481C1.60906 30.5692 0.98828 27.4483 0.988281 24.2966C0.988282 21.1449 1.60906 18.024 2.81518 15.1122C4.02129 12.2004 5.78912 9.55465 8.01772 7.32605C10.2463 5.09744 12.8921 3.32961 15.8039 2.1235C18.3457 1.07064 21.0469 0.463815 23.7888 0.326608C24.4507 0.293486 24.9883 0.833874 24.9883 1.49662C24.9883 2.15936 24.4506 2.69314 23.7889 2.72994C21.3623 2.86489 18.9729 3.40858 16.7223 4.34082C14.1017 5.42632 11.7205 7.01736 9.71478 9.0231C7.70904 11.0288 6.11799 13.41 5.03249 16.0306C3.94698 18.6513 3.38828 21.4601 3.38828 24.2966C3.38828 27.1332 3.94698 29.9419 5.03248 32.5626C6.11798 35.1832 7.70903 37.5644 9.71477 39.5701C11.7205 41.5759 14.1017 43.1669 16.7223 44.2524C18.9729 45.1846 21.3623 45.7283 23.7889 45.8633C24.4506 45.9001 24.9883 46.4339 24.9883 47.0966Z"
-            fill="#0DDD60"
-          />
-        </svg>
+        <img className="h-full" src={loader1} />
       </motion.div>
       <motion.div
-        className="absolute"
-        style={{
-          width: innerSize,
-          height: innerSize,
-          left: innerOffset,
-          top: innerOffset,
-        }}
+        style={innerSvgStyle}
         animate={{ rotate: -360 }}
         transition={{ ease: "linear", duration: 2.2, repeat: Infinity }}
       >
-        <svg
-          className="h-full ml-auto"
-          width="14"
-          height="27"
-          viewBox="0 0 14 27"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M0.988281 1.59662C0.988281 0.878645 1.57174 0.289883 2.28613 0.361562C3.54809 0.488182 4.78711 0.799042 5.96317 1.28618C7.5404 1.93949 8.97351 2.89707 10.1807 4.10423C11.3878 5.31139 12.3454 6.7445 12.9987 8.32173C13.652 9.89897 13.9883 11.5894 13.9883 13.2966C13.9883 15.0038 13.652 16.6943 12.9987 18.2715C12.3454 19.8487 11.3878 21.2818 10.1807 22.489C8.97351 23.6962 7.5404 24.6537 5.96316 25.3071C4.7871 25.7942 3.54808 26.1051 2.28612 26.2317C1.57174 26.3033 0.988277 25.7146 0.988277 24.9966C0.988277 24.2786 1.57254 23.705 2.2849 23.6155C3.20513 23.4998 4.10772 23.2614 4.96819 22.905C6.22997 22.3823 7.37646 21.6163 8.34219 20.6505C9.30792 19.6848 10.074 18.5383 10.5966 17.2765C11.1193 16.0147 11.3883 14.6624 11.3883 13.2966C11.3883 11.9309 11.1193 10.5785 10.5966 9.31671C10.074 8.05492 9.30792 6.90843 8.34219 5.94271C7.37646 4.97698 6.22998 4.21092 4.96819 3.68827C4.10773 3.33185 3.20514 3.09339 2.28491 2.97776C1.57254 2.88825 0.988281 2.31459 0.988281 1.59662Z"
-            fill="#4DA6FF"
-          />
-        </svg>
+        <img className="h-full ml-auto" src={loader2} />
       </motion.div>
     </div>
   );
